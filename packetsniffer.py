aafb3e29bf2f97fc31a06d5e1ea911167d01dc96
Dheeraj Sharma <xsharmax60062@gmail.com>
2026-04-06 22:29:05 +0530

    Add basic packet sniffer functionality
    Implement a basic packet sniffer using raw sockets.

---
+++ packetsniffer.py
@@ -0,0 +1,23 @@
+# Day 7: Basic Packet Sniffer
+
+import socket
+
+def packet_sniffer():
+    # Create raw socket
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+
+    # Get local host IP
+    host = socket.gethostbyname(socket.gethostname())
+
+    # Bind socket
+    sniffer.bind((host, 0))
+
+    # Include IP headers
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+    # Enable promiscuous mode
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+    print(f"Listening on {host}...\n")
+
+   
