72eb042c31c0954e8f0bab5bfe01bb4cd89aa25f
Dheeraj Sharma <xsharmax60062@gmail.com>
2026-04-18 21:52:23 +0530

    Implement CLI To-Do App with file storage

---
+++ todo.py
@@ -0,0 +1,69 @@
+# Day 20: CLI To-Do App (with file storage)
+
+import os
+
+FILE = "tasks.txt"
+
+def load_tasks():
+    if not os.path.exists(FILE):
+        return []
+    with open(FILE, "r") as f:
+        return [line.strip() for line in f.readlines()]
+
+def save_tasks(tasks):
+    with open(FILE, "w") as f:
+        for task in tasks:
+            f.write(task + "\n")
+
+def show_tasks(tasks):
+    if not tasks:
+        print("\nNo tasks found!")
+    else:
+        print("\nYour Tasks:")
+        for i, task in enumerate(tasks, start=1):
+            print(f"{i}. {task}")
+
+def add_task(tasks):
+    task = input("Enter new task: ")
+    tasks.append(task)
+    save_tasks(tasks)
+    print("Task added!")
+
+def delete_task(tasks):
+    show_tasks(tasks)
+    try:
+        num = int(input("Enter task number to delete: "))
+        if 1 <= num <= len(tasks):
+            removed = tasks.pop(num - 1)
+            save_tasks(tasks)
+            print(f"Deleted: {removed}")
+        else:
+            print("Invalid number")
+    except:
+        print("Invalid input")
+
+def main():
+    tasks = load_tasks()
+
+    while True:
+        print("\n===== TO-DO MENU =====")
+        print("1. View Tasks")
+        print("2. Add Task")
+        print("3. Delete Task")
+        print("4. Exit")
+
+        choice = input("Enter choice: ")
+
+        if choice == "1":
+            show_tasks(tasks)
+        elif choice == "2":
+            add_task(tasks)
+        elif choice == "3":
+            delete_task(tasks)
+        elif choice == "4":
+            break
+        else:
+            print("Invalid choice!")
+
+if __name__ == "__main__":
+    main()
